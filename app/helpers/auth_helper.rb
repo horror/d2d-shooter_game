407b289c98e16c720ee285ed6e557d0f90514ded
horror <spinhorror@gmail.ru>
2013-09-25 11:25:49 +1100

    code refactoring

--- app/helpers/auth_helper.rb
+++ app/helpers/auth_helper.rb
@@ -10,24 +10,26 @@ module AuthHelper
 
   def signin(params)
     user = User.find_by_login(params["login"].downcase)
-    if user && user.authenticate(params["password"]) ? "ok" : "incorrect"
-      user.sid = SecureRandom.urlsafe_base64
-      user.save
-      self.response_obj = {result: "ok", sid: user.sid}
-    else
+    if !user || !user.authenticate(params["password"])
       self.response_obj = {result: "incorrect"}
+      return
     end
+
+    user.sid = SecureRandom.urlsafe_base64
+    user.save
+    self.response_obj = {result: "ok", sid: user.sid}
   end
 
   def signout(params)
-    user = User.find_by_sid(params["sid"])
-    if user
-      user.sid = ''
-      user.save
-      self.response_obj = {result: "ok"}
-    else
+
+    if not (user = User.find_by_sid(params["sid"]))
       self.response_obj = {result: "badSid"}
+      return
     end
+
+    user.sid = ''
+    user.save
+    self.response_obj = {result: "ok"}
   end
 
 
--- app/helpers/chat_helper.rb
+++ app/helpers/chat_helper.rb
@@ -1,37 +1,42 @@
 module ChatHelper
   def sendMessage(params)
-    user = User.find_by_sid(params["sid"])
-    if user
-      new_message_params = {game_id: params["game"], user_id: user.id, text: params[:text]}
-      message = Message.new(new_message_params)
-      msg = message.save && Game.find_by_id(params["game"]) ? "ok" : "badGame"
-      self.response_obj = {result: msg}
-    else
+    if not (user = User.find_by_sid(params["sid"]))
       self.response_obj = {result: "badSid"}
+      return
     end
+
+    if not Game.find_by_id(params["game"])
+      self.response_obj = {result: "badGame"}
+      return
+    end
+
+
+    new_message_params = {game_id: params["game"], user_id: user.id, text: params[:text]}
+    message = Message.new(new_message_params)
+    self.response_obj = {result: "ok"}
   end
 
   def getMessages(params)
-    user = User.find_by_sid(params["sid"])
-    if user
-      condition = ["m.created_at > ?", params["since"]]
-      if params["game"]
-        if not (game = Game.find_by_id(params["game"]))
-          self.response_obj = {result: "badGame"}
-          return
-        end
-        condition = ["m.game_id = ? AND m.created_at > ?", game.id, params["since"]]
-      end
-      messages = Message.all(
-                :select => "u.login AS login, m.text, m.created_at AS time",
-                :from => 'messages m',
-                :joins => "INNER JOIN users u ON m.user_id = u.id",
-                :conditions => condition,
-                :order => 'm.created_at desc',
-      ).to_a
-      self.response_obj = {result: "ok", login: user.login, message: messages}
-    else
+    if not (user = User.find_by_sid(params["sid"]))
       self.response_obj = {result: "badSid"}
+      return
     end
+
+    condition = ["m.created_at > ?", params["since"]]
+    if params["game"]
+      if not (game = Game.find_by_id(params["game"]))
+        self.response_obj = {result: "badGame"}
+        return
+      end
+      condition = ["m.game_id = ? AND m.created_at > ?", game.id, params["since"]]
+    end
+    messages = Message.all(
+              :select => "u.login AS login, m.text, m.created_at AS time",
+              :from => 'messages m',
+              :joins => "INNER JOIN users u ON m.user_id = u.id",
+              :conditions => condition,
+              :order => 'm.created_at desc',
+    ).to_a
+    self.response_obj = {result: "ok", login: user.login, message: messages}
   end
 end
