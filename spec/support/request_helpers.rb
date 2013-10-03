3e3b683d4be1323b2878b3b43c9c88389ff57424
VeN <ven91@mail.ru>
2013-10-03 22:39:40 +1100

    Request helper changes Functions have been renamed. Added new.

--- spec/support/request_helpers.rb
+++ spec/support/request_helpers.rb
@@ -1,15 +1,20 @@
 module Requests
   module JsonHelpers
-    def enc(obj)
+    def json_encode(obj)
       ActiveSupport::JSON.encode(obj)
     end
 
-    def dec(str)
+    def json_decode(str)
       ActiveSupport::JSON.decode(str)
     end
 
     def send_request(obj)
-      xhr :post, "/", enc(obj), "CONTENT_TYPE" => 'application/json; charset=utf-8', "DATA_TYPE" => 'json'
+      xhr :post, "/", json_encode(obj), "CONTENT_TYPE" => 'application/json; charset=utf-8', "DATA_TYPE" => 'json'
+    end
+
+    def request_and_checking(action, params, body = {result: "ok"}, code = "200")
+      send_request(action: action, params: params)
+      response.code.should == code && response.body.should == json_encode(body)
     end
   end
 end
