1808116f320d7e9c436e0ffab6a9931c49908ac0
VeN <ven91@mail.ru>
2013-10-22 15:10:57 +1100

    Added simple handling of acceleration/deceleration

--- lib/messenger.rb
+++ lib/messenger.rb
@@ -1,10 +1,14 @@
 RESPAWN = "$"
+WALL = "#"
 MOVE = "move"
+DEFAULT_ACCELERATION = 5
 
 class Messenger
   def initialize(ws)
     @ws = ws
-    @to_hash = Hash.new
+    @to_hash = {vx: 0.0, vy: 0.0, x: 0.0, y: 0.0}
+    @changed = false
+    @initialized = false
   end
 
   # запуск бесконечного цикла
@@ -22,6 +26,14 @@ class Messenger
     !@stopped
   end
 
+  def changed?
+    @changed
+  end
+
+  def no_changes
+    @changed = false
+  end
+
   def game
     (@game_id ? @game_id : 0).to_s
   end
@@ -31,12 +43,14 @@ class Messenger
   end
 
   def on_message(players, ws = nil)
+    deceleration if not changed?
+    no_changes
     @ws.send(ActiveSupport::JSON.encode(players.values)) if players and running? and (ws.nil? or @ws == ws)
   end
 
   def process(data, ws, players, items)
     return if @ws != ws || !(user = User.find_by_sid(data["sid"])) || !(player = Player.find_by_user_id(user.id))
-
+    @changed = true
     @sid ||= data["sid"]
     if !@game_id #определяем игру для клиента, если еще небыла определена
       @game_id = player.game_id
@@ -44,37 +58,60 @@ class Messenger
       players[game][@sid] = to_hash
     end
 
-    if !@to_hash[:x] #если координаты клиента не определены, находим координаты респаунов и присваеваем случайный клиенту
+    if !@initialized #если координаты клиента не определены, находим координаты респаунов и присваеваем случайный клиенту
       if !items[game] #если предметы на карте в этой игре не определены, определяем их
         items[game] = Hash.new
         items[game]["respawns"] = Array.new
+        items[game]["walls"] = Array.new
         map = ActiveSupport::JSON.decode(player.game.map.map)
-
-        for i in 0..(map.size - 1)
-          for j in 0..(map[0].length - 1)
+        @bottom_bound = (map.size - 1).to_f
+        @left_bound = (map[0].length - 1).to_f
+        for i in 0..@bottom_bound
+          for j in 0..@left_bound
             case map[i][j]
               when RESPAWN
-                items[game]["respawns"] << {x: j, y: i}
+                items[game]["respawns"] << Geometry::Point[j, i]
             end
           end
         end
       end
 
       resp = items[game]["respawns"][rand(items[game]["respawns"].size)]
-      set_coords(resp[:x], resp[:y])
+      set_position(resp.x, resp.y)
+      @initialized = true
     end
 
     send(data["action"], data)
   end
 
-  def set_coords(x, y)
+  def set_position(x, y)
     @to_hash[:x] = x
     @to_hash[:y] = y
   end
 
-  def move_coords(dx, dy)
-    @to_hash[:x] += dx
-    @to_hash[:y] += dy
+  def move_position
+    @to_hash[:x] += @to_hash[:vx]
+    @to_hash[:y] += @to_hash[:vy]
+    set_position([[0.0, @to_hash[:x]].max, @left_bound].min, [[0.0, @to_hash[:y]].max, @bottom_bound].min)
+  end
+
+  def normalize(dx, dy)
+    if (max = [dx.to_f.abs, dy.to_f.abs].max) != 0.0
+      dx /= max
+      dy /= max
+    end
+    return dx, dy
+  end
+
+  def change_velocity(dx, dy)
+    dx, dy = normalize(dx, dy)
+    @to_hash[:vx] += dx * DEFAULT_ACCELERATION
+    @to_hash[:vy] += dy * DEFAULT_ACCELERATION
+  end
+
+  def deceleration
+    change_velocity(-@to_hash[:vx], -@to_hash[:vy])
+    move_position
   end
 
   def to_hash
@@ -83,6 +120,7 @@ class Messenger
 
   ###ACTIONS###
   def move(data)
-    move_coords(data["dx"], data["dy"])
+    change_velocity(data["dx"], data["dy"])
+    move_position
   end
 end
