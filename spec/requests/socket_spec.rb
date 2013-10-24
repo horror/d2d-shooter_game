b6d0309a32f083647fcc4e45685aa66110e6bf56
VeN <ven91@mail.ru>
2013-10-25 07:37:28 +1100

    Changes at websocket test

--- spec/requests/socket_spec.rb
+++ spec/requests/socket_spec.rb
@@ -3,10 +3,13 @@ require 'em-websocket-request'
 
 describe 'Socket server' do
 
-  sid_a = sid_b = ""
-  map_id = game_id = 0
   TEST_HOST = 'localhost'
   TEST_PORT = ':8080'
+  EPS = 1e-7
+
+  sid_a = sid_b = ""
+  map_id = game_id = 0
+  check_arr = {'vx' => 0.0, 'vy' => 0.0, 'x' => 2.5, 'y' => 0.5}
 
   def webSocketRequest()
     request = EventMachine::WebsocketRequest.new('ws://' + TEST_HOST + TEST_PORT).get
@@ -18,17 +21,6 @@ describe 'Socket server' do
     return request
   end
 
-  def getCheckArr(y)
-    case y
-      when 0.5
-        return {'vx' => 0.0, 'vy' => 0.0, 'x' => 1.5, 'y' => 0.5}
-      when 2.5
-        return {'vx' => 0.0, 'vy' => 0.0, 'x' => 0.5, 'y' => 2.5}
-      when 3.5
-        return {'vx' => 0.0, 'vy' => 0.0, 'x' => 2.5, 'y' => 3.5}
-    end
-  end
-
   before(:all) do
     send_request(action: "startTesting", params: {})
     send_request(action: "signup", params: {login: "user_a", password: "password"})
@@ -38,7 +30,7 @@ describe 'Socket server' do
     send_request(action: "signin", params: {login: "user_b", password: "password"})
     sid_b = json_decode(response.body)["sid"]
     send_request(action: "uploadMap", params: {sid: sid_a, name: "New map", maxPlayers: 10,
-                                               map: ['1$.2.', '.3#1.', '$3.#.', '2.$.#']})
+                                               map: ['1.$.2', '#3.1.', '#3.#.', '2...#']})
     send_request(action: "getMaps", params: {sid: sid_a})
     map_id = json_decode(response.body)["maps"][0]["id"]
     send_request(action: "createGame", params: {sid: sid_a, name: "New game", map: map_id, maxPlayers: 10})
@@ -48,39 +40,55 @@ describe 'Socket server' do
     send_request(action: "joinGame", params: {sid: sid_b, game: game_id})
   end
 
-  it "player connection, random spawn" do
+  it "player spawn" do
     EM.run do
       request = webSocketRequest()
-      request.callback { request.send(json_encode({sid: sid_a, action: "move", dx: 0, dy: 0})) }
 
-      request.stream { |chunk, type|
-        #msg = request.process_data(chunk, type)
-        arr = json_decode(chunk)[0]
-        [0.5, 2.5, 3.5].include?(arr['y']).should == true
-        check_arr = getCheckArr(arr['y'])
-        arr.should == check_arr
+      request.callback {
+        request.send(json_encode({sid: sid_a, action: "move", dx: 0, dy: 0}))
+      }
+
+      request.stream { |message, type|
+        json_decode(message)['players'][0].should == check_arr
         EM.stop_event_loop
       }
     end
   end
 
-  it "action move" do
+  it "action one step move" do
     EM.run do
       request = webSocketRequest()
+
       request.callback {
-        for i in 0..10 do
-          request.send(json_encode({sid: sid_a, action: "move", dx: 1, dy: 0}))
+        request.send(json_encode({sid: sid_a, action: "move", dx: 1, dy: 1}))
+        request.send(json_encode({sid: sid_a, action: "move", dx: 1, dy: 1}))
+      }
+
+      num = 0
+      request.stream { |message, type|
+        arr = json_decode(message)
+        arr['players'][0]['y'].should > check_arr['y']
+        arr['players'][0]['x'].should > check_arr['x']
+        EM.stop_event_loop
+      }
+    end
+  end
+
+  it "player teleport" do
+    EM.run do
+      request = webSocketRequest()
+
+      request.callback {
+        for i in 0..10
+          request.send(json_encode({sid: sid_a, action: "move", dx: 1, dy: 1}))
         end
       }
-      counter = 0
-      check_arr = {}
-      request.stream { |chunk, type|
-        counter += 1
-        check_arr = getCheckArr(json_decode(chunk)[0]["y"]) if counter == 1
-        if counter == 20
-          arr = json_decode(chunk)[0]
-          arr['y'].should == check_arr['y']
-          arr['x'].should > check_arr['x'] + 0.5
+
+      request.stream { |message, type|
+        arr = json_decode(message)['players'][0]
+        if arr['vx'] < EPS
+          arr['y'].should == 3.5
+          (arr['x'].should - 4.5).should < EPS
           EM.stop_event_loop
         end
       }
