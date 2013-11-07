785b2db05835a92abb0ef9d2c74b81c0e645a02f
VeN <ven91@mail.ru>
2013-11-08 04:39:45 +1100

    Client class changes Fix tp bag by remembering coords of last tp. Remove functions of velocity's handling. Fix move_position(added 0.5 offset in colculations, zero velocity in collision with border etc.) Add max velocity.

--- lib/Client.rb
+++ lib/Client.rb
@@ -3,18 +3,40 @@ VOID = "."
 WALL = "#"
 MOVE = "move"
 DEFAULT_ACCELERATION = 0.1
-EPSILON = 0.0000001
+EPSILON = 1e-7
 ACCURACY = 6
+MAX_VELOCITY = 1
+
+def v_sign(num)
+  return num.to_f == 0.0 ? 0.0 : num.to_f > 0.0 ? 1 : -1
+end
+
+def normalize(dx, dy)
+  return 0, 0 if (norm = Math.sqrt(dx.to_f**2 + dy.to_f**2)) == 0.0
+  dx /= norm
+  dy /= norm
+  return dx, dy
+end
+
+def new_velocity(dx, dy, vx, vy)
+  dx, dy = normalize(dx, dy)
+  #puts "login = #{@login}, vx = #{vx}, DX = #{dx * DEFAULT_ACCELERATION}, vx + DX = #{[(vx + dx * DEFAULT_ACCELERATION).round(ACCURACY), MAX_VELOCITY].min}"
+  vx, vy = (vx + dx * DEFAULT_ACCELERATION).round(ACCURACY), (vy + dy * DEFAULT_ACCELERATION).round(ACCURACY)
+  return [vx.abs, MAX_VELOCITY].min * v_sign(vx), [vy.abs, MAX_VELOCITY].min * v_sign(vy)
+end
 
 class Client
+
   def initialize(players, items, maps)
     @player = {vx: 0.0, vy: 0.0, x: 0.0, y: 0.0, hp: 100}
     @changed = false
     @initialized = false
-    @teleported = false
+    @last_tp = {x: -1, y: -1}
     @players = players
     @maps = maps
     @items = items
+
+    @login = ""
   end
 
   def changed?
@@ -43,6 +65,7 @@ class Client
   def process(data, tick)
     params = data['params']
     return if !(user = User.find_by_sid(params["sid"])) || !(player = Player.find_by_user_id(user.id)) || (@initialized && tick > params['tick'])
+    @login = user.login
     @changed = true
     @sid ||= params["sid"]
 
@@ -98,43 +121,47 @@ class Client
   end
 
   def move_position
-    symbol = @maps[game][y = (@player[:y] + @player[:vy]).floor][x = (@player[:x] + @player[:vx]).floor]
-    @teleported = false if symbol == VOID or symbol == RESPAWN
-    make_tp(x, y) if ("0".."9").include?(symbol) && !@teleported
-    stop_movement if symbol == WALL
+    x, y = (@player[:x] + @player[:vx]).floor, (@player[:y] + @player[:vy]).floor
+    symbol = x < 0.0 || y < 0.0 ? "" : @maps[game][y][x]
+    @last_tp = {x: -1, y: -1} if symbol == VOID or symbol == RESPAWN
+    #puts "symbol = " + symbol, "istp = " + @teleported.to_s, "x ,y = " + x.to_s + ", " + y.to_s
+    if ("0".."9").include?(symbol) && @last_tp != {x: x, y: y}
+      make_tp(x, y)
+      return
+    end
 
-    @player[:x] += @player[:vx]
-    @player[:y] += @player[:vy]
-    set_position([[0.0, @player[:x]].max, @right_bound].min, [[0.0, @player[:y]].max, @bottom_bound].min)
+    x = (@player[:x] + @player[:vx] + v_sign(@player[:vx]) * 0.5 + EPSILON*v_sign(@player[:vx])).floor
+    y = (@player[:y] + @player[:vy] + v_sign(@player[:vy]) * 0.5 + EPSILON*v_sign(@player[:vx])).floor
+    symbol = x < 0.0 || y < 0.0 ? "" : @maps[game][y][x]
+    x = v_sign(@player[:vx]) == 0 ? @player[:x] : v_sign(@player[:vx]) > 0 ? x - 0.5 : x + 1.5
+    y = v_sign(@player[:vy]) == 0 ? @player[:y] : v_sign(@player[:vy]) > 0 ? y + 1.5 : y - 0.5
+    if symbol == WALL
+      stop_movement(x, y)
+      return
+    end
+
+    set_position(@player[:x] + @player[:vx], @player[:y] + @player[:vy])
+
+    if @player[:x] < 0.5 || @player[:x] > @right_bound - 0.5 || @player[:y] < 0.5 || @player[:y] > @bottom_bound - 0.5
+      stop_movement([[0.5, @player[:x]].max, @right_bound - 0.5].min, [[0.5, @player[:y]].max, @bottom_bound - 0.5].min)
+    end
   end
 
   def make_tp(x, y)
     tps = @items[game]["teleports"][@maps[game][y][x]]
-    tp = (tps[0][:x] == x and tps[0][:y] == y ? tps[1] : tps[0])
+    tp = tps[0][:x] == x && tps[0][:y] == y ? tps[1] : tps[0]
     set_position(tp[:x].to_f + 0.5, tp[:y].to_f + 0.5)
-    @teleported = true
+    @last_tp = tp
   end
 
-  def normalize(dx, dy)
-    return 0, 0 if (norm = Math.sqrt(dx.to_f**2 + dy.to_f**2)) == 0.0
-    dx /= norm
-    dy /= norm
-    return dx, dy
-  end
-
-  def change_velocity(dx, dy)
-    dx, dy = normalize(dx, dy)
-    @player[:vx] = (@player[:vx] + dx * DEFAULT_ACCELERATION).round(ACCURACY)
-    @player[:vy] = (@player[:vy] + dy * DEFAULT_ACCELERATION).round(ACCURACY)
-  end
-
-  def stop_movement
+  def stop_movement(x, y)
     @player[:vx] = 0.0
     @player[:vy] = 0.0
+    set_position(x, y)
   end
 
   def deceleration
-    change_velocity(-@player[:vx], -@player[:vy])
+    @player[:vx], @player[:vy] = new_velocity(-@player[:vx], -@player[:vy], @player[:vx], @player[:vy])
     move_position
   end
 
@@ -144,7 +171,7 @@ class Client
 
   ###ACTIONS###
   def move(data)
-    change_velocity(data["dx"], data["dy"])
+    @player[:vx], @player[:vy] = new_velocity(data["dx"], data["dy"], @player[:vx], @player[:vy])
     move_position
   end
 end
