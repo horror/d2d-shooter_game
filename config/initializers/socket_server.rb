94a0784c7e2a8ae8e96c076b664e387bf3023ef2
horror <spinhorror@gmail.ru>
2013-11-15 00:44:29 +1100

    Websoket refactoring: add ActiveGame class, used attr_accessor with Client

--- config/initializers/socket_server.rb
+++ config/initializers/socket_server.rb
@@ -2,24 +2,22 @@ require 'em-websocket'
 
 EM.next_tick do
   @clients = Hash.new
-  @pt
-  @players = {"0" => []}
-  @maps = Hash.new
-  @items = Hash.new
+  @timer
   @tick = 0
-  @answered_players = {"0" => []}
+  @games = Hash.new
 
-  EM::WebSocket.start(:host => '0.0.0.0', :port => 8080) do |ws|
+  EM::WebSocket.start(:host => '0.0.0.0', :port => 8001) do |ws|
     ws.onopen do
-      @clients[ws] = Client.new(@players, @items, @maps)
+      @clients[ws] = Client.new(ws, @games)
+
       if !ValidationHelper::synchron_websocket?
-        @pt ||= EM::PeriodicTimer.new(0.03) do
+        @timer ||= EM::PeriodicTimer.new(0.03) do
           if @clients.empty?
-            @pt.cancel
-            @pt = nil
+            @timer.cancel
+            @timer = nil
           end
           @tick += 1
-          @clients.each { |ws_handler, client| client.on_message(ws_handler, @players[client.game], @tick) }
+          @clients.each { |ws_handler, client| client.on_message(@tick) }
         end
       end
     end
@@ -27,22 +25,25 @@ EM.next_tick do
     ws.onmessage do |msg|
       msg = ActiveSupport::JSON.decode(msg)
       @clients[ws].process(msg, @tick) if msg["action"] != "empty"
-      if ValidationHelper::synchron_websocket?
-        @answered_players[@clients[ws].game] ||= Hash.new
-        all_answered = @answered_players[@clients[ws].game][msg['params']['sid']] = true
-        @answered_players[@clients[ws].game].each{|sid, val| all_answered &= val}
+
+      if ValidationHelper::synchron_websocket? && @clients[ws].game
+        answered_players = @clients[ws].game.answered_players
+        all_answered = answered_players[msg['params']['sid']] = true
+        answered_players.each{|sid, val| all_answered &= val}
         if all_answered
           @tick += 1
-          @answered_players[@clients[ws].game].each{|sid, val| @answered_players[@clients[ws].game][sid] = false}
-          @clients.each { |ws_handler, client| client.on_message(ws_handler, @players[client.game], @tick) if client.game == @clients[ws].game}
+          answered_players.each{|sid, val| answered_players[sid] = false}
+          @clients.each {|ws_handler, client| client.on_message(@tick) if client.game_id == @clients[ws].game_id}
         end
       end
     end
 
     ws.onclose do
       puts "WebSocket closed"
-      @players[@clients[ws].game].delete(@clients[ws].sid)
-      @answered_players[@clients[ws].game].delete(@clients[ws].sid) if ValidationHelper::synchron_websocket?
+      if @clients[ws].game
+        @clients[ws].game.answered_players.delete(@clients[ws].sid) if ValidationHelper::synchron_websocket?
+        @clients[ws].game.players.delete(@clients[ws].sid)
+      end
       @clients.delete(ws)
     end
   end
