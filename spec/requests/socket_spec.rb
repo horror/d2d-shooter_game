f3f68869fcb1e140497dcf03edb74eea637a3f7c
VeN <ven91@mail.ru>
2013-12-08 00:25:18 +1100

    Add new WS tests for moves and collisions with gravity

--- spec/requests/socket_spec.rb
+++ spec/requests/socket_spec.rb
@@ -187,35 +187,122 @@ describe 'Socket server' do
     end
   end
 
-  describe "Gravity, respawns order" do
+  def recreate_game(map, sid_a, sid_b, map_id, game_id, index)
+    send_request(action: "leaveGame", params: {sid: sid_a})
+    send_request(action: "leaveGame", params: {sid: sid_b})
+    send_request(action: "uploadMap", params: {sid: sid_a, name: "New map #{index}", maxPlayers: 10, map: map})
+    send_request(action: "getMaps", params: {sid: sid_a})
+    map_id = json_decode(response.body)["maps"]
+    map_id = map_id[map_id.size - 1]["id"]
+    send_request(action: "createGame", params: {sid: sid_a, name: "New game #{index}", map: map_id, maxPlayers: 10})
+    send_request(action: "getGames", params: {sid: sid_a})
+    game_id = json_decode(response.body)["games"]
+    game_id = game_id[game_id.size - 1]["id"]
+    send_request(action: "joinGame", params: {sid: sid_b, game: game_id})
+  end
+
+  describe "Gravity: " do
+
+    spawns = [{coord: Point(5.5, 0.5), velocity: Point(0, 0)}, {coord: Point(0.5, 3.5), velocity: Point(0, 0)},
+              {coord: Point(7.5, 3.5), velocity: Point(0, 0)}]
 
     before(:all) do
-      send_request(action: "leaveGame", params: {sid: sid_a})
-      send_request(action: "leaveGame", params: {sid: sid_b})
-      send_request(action: "uploadMap", params: {sid: sid_a, name: "New map 2", maxPlayers: 10,
-                                                 map: ['1$...$..',
-                                                       '####.#..',
-                                                       '..1....$',
-                                                       '###.2###',
-                                                       '........',
-                                                       '........',
-                                                       '2.......',
-                                                       '#......#',
-                                                       '###.....']})
-      send_request(action: "getMaps", params: {sid: sid_a})
-      map_id = json_decode(response.body)["maps"][1]["id"]
-      send_request(action: "createGame", params: {sid: sid_a, name: "New game 2", map: map_id, maxPlayers: 10})
-      send_request(action: "getGames", params: {sid: sid_a})
-      game_id = json_decode(response.body)["games"][1]["id"]
-      send_request(action: "joinGame", params: {sid: sid_b, game: game_id})
+      map = ['.....$..',
+             '####.#..',
+             '........',
+             '$......$',
+             '#.#..###']
+      recreate_game(map, sid_a, sid_b, map_id, game_id, 2)
     end
 
-    it "respawns" do
-      EM.run{ def_request( {sid: sid_a, check_limit: 0, send_limit: 0, x: 1.5, y: 0.5} ) }
-      EM.run{ def_request( {sid: sid_a, check_limit: 0, send_limit: 0, x: 5.5, y: 0.5} ) }
+    it "respawns order" do
+      EM.run{ def_request( {sid: sid_a, x: spawns[0][:coord].x, y: spawns[0][:coord].y} ) }
+      EM.run{ def_request( {sid: sid_a, x: spawns[1][:coord].x, y: spawns[1][:coord].y} ) }
       EM.run{
-        def_request( {sid: sid_a, check_limit: 0, send_limit: 0, x: 7.5, y: 2.5} )
-        def_request( {index: 1, sid: sid_b, check_limit: 0, send_limit: 0, x: 1.5, y: 0.5} )
+        def_request( {sid: sid_a, check_limit: 5, x: spawns[2][:coord].x, y: spawns[2][:coord].y} )
+        def_request( {index: 1, sid: sid_b, x: spawns[0][:coord].x, y: spawns[0][:coord].y} )
+      }
+    end
+
+    it "jump along the wall and go to left corner" do
+      EM.run{
+        def_request( {sid: sid_a, check_limit: 15, send_limit: 1, x: spawns[1][:coord].x, y: spawns[1][:coord].y, dy_rule: -1} )
+      }
+    end
+
+    it "jump and fall" do
+      EM.run do
+        p_tick = 0
+        request = web_socket_request(sid_a)
+        curr_player = spawns[2]
+        request.stream { |message, type|
+          p_tick += 1
+          arr = json_decode(message)
+          player = arr['players'][0]
+          should_eql(player['y'], curr_player[:coord].y, "coord.y")
+          should_eql(player['vy'], curr_player[:velocity].y, "velocity.y")
+          if p_tick == 1
+            send_ws_request(request, "move", {sid: sid_a, dx: 0, dy: -1, tick: arr['tick']})
+            curr_player[:coord].y += (curr_player[:velocity].y = -game_consts[:max_velocity])
+            next
+          end
+          curr_player[:coord].y += (curr_player[:velocity].y += game_consts[:gravity])
+          send_ws_request(request, "empty", {sid: sid_a, tick: arr['tick']})
+          close_socket(request, sid_a) if p_tick == 22
+        }
+      end
+    end
+
+    it "go out the floor" do
+      EM.run{
+        def_request( {sid: sid_a, check_limit: 10, send_limit: 10, x: 7.5, y: 1, vy: 0.2, dx_rule: 1} )
+      }
+    end
+
+    it "fall to narrow" do
+      EM.run{
+        def_request( {sid: sid_a, check_limit: 45, send_limit: 45, x: 1.5, y: 4.5,
+                      action: Proc.new{|p_tick| p_tick % 2 == 0 ? "move" : "empty"}, dx_rule: 1} )
+      }
+    end
+
+    it "jump to left corner" do
+      EM.run{
+        def_request( {sid: sid_a, check_limit: 7, send_limit: 6, x: 6.5, y: 2.5, vx: 0, vy: 0,
+                      dx_rule: Proc.new{|p_tick| p_tick < 4 ? -1 : 0}, dy_rule: Proc.new{|p_tick| p_tick == 4 ? -1 : 0}} )
+      }
+    end
+
+    it "exception collison by left bottom corner" do
+      action = Proc.new{|p_tick, player|
+        next p_tick % 2 == 0 ? "move" : "empty" if player["x"] > 4.5
+        "move"
+      }
+      EM.run{
+        def_request( {sid: sid_a, check_limit: 40, send_limit: 40, vx: -0.1, x: 4.4, y: 0.5, action: action, dx_rule: -1} )
+      }
+    end
+
+    it "exception collison by right bottom corner" do
+      action = Proc.new{|p_tick, player|
+        next p_tick % 2 == 0 ? "move" : "empty" if player["x"] < 1.5
+        "move"
+      }
+      EM.run{
+        def_request( {sid: sid_a, check_limit: 40, send_limit: 40, vx: 0.1, x: 1.6, y: 3.5, action: action, dx_rule: 1} )
+      }
+    end
+
+    it "collision with a small left portion of the bottom edge of wall" do
+      dx_rule = Proc.new{|p_tick| p_tick < 10 ? -1 : 1}
+      dy_rule = Proc.new{|p_tick| p_tick == 9 ? -1 : 0}
+      EM.run{
+        def_request( {log: 1, sid: sid_a, check_limit: 13, send_limit: 13, dx_rule: dx_rule, dy_rule: dy_rule} ){ |player|
+          player['x'].should < 4.5 #ударились головой, пролетели влево
+          player['vx'].should < -0.2
+          should_eql(player['y'], 2.55, "coord.y")
+          should_eql(player['vy'], 0.05, "velocity.y")
+        }
       }
     end
   end
