fdd5d2ecad72a8c0dd4cb71c50c61b32c42efe37
Alex <spinhorror@gmail.com>
2014-08-05 13:34:40 +1100

    fix bag registration with number only password

--- app/helpers/auth_helper.rb
+++ app/helpers/auth_helper.rb
@@ -6,8 +6,8 @@ module AuthHelper
   end
 
   def signin(params)
-    raise BadParamsError.new(badLogin) unless params["login"].kind_of?(String)
-    raise BadParamsError.new(badPassword) unless params["password"].kind_of?(String)
+    #raise BadParamsError.new(badLogin) unless params["login"].kind_of?(String)
+    #raise BadParamsError.new(badPassword) unless params["password"].kind_of?(String)
     user = User.find_by_login(params["login"])
     raise BadParamsError.new(incorrect) unless user and user.authenticate(params["password"])
     user.update_attribute(:sid, SecureRandom.urlsafe_base64)
