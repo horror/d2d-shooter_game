eed95e9aabbf018344aaa2f123bf76a4d20c3da0
Александр <spinhorror@mail.ru>
2015-11-28 22:26:14 +1000

    delete gemfile.lock from repo, fix periodic timer param

--- config/initializers/socket_server.rb
+++ config/initializers/socket_server.rb
@@ -69,10 +69,11 @@ module WS
 end
 
 EM.next_tick do
+  EM.set_quantum(5)
   EM.set_timer_quantum(5)
   @start = @next = Time.now.to_f
   @interval = Settings.tick_size / 1000
-  EM.add_periodic_timer(@interval / 30) do
+  EM.add_periodic_timer(@interval / 5) do
     if Time.now.to_f >= @next
       @next = WS.inc_tick * @interval + @start
       WS.send_clients_response(nil)
